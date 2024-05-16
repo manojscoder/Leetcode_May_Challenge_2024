f1cc6a8068c0215da414ad25185f234970adaa6d
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-05-16 22:15:48 +0530

    Update Day_16.py

--- Day_16.py
+++ Day_16.py
@@ -1,6 +1,8 @@
 # Problem link: https://leetcode.com/problems/evaluate-boolean-binary-tree/description
 # Time: O(n)
-# Space: O(n) for recursion call stack
+# Space: O(n)
+
+# Recursive approach
 class Solution:
     def evaluateTree(self, node: Optional[TreeNode]) -> bool:
         if not node.left:
@@ -9,3 +11,22 @@ class Solution:
         if node.val == 2:
             return self.evaluateTree(node.left) or self.evaluateTree(node.right)
         return self.evaluateTree(node.left) and self.evaluateTree(node.right)
+
+
+
+# Iterative approach
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        stack, store = [root], {}
+        
+        while stack:
+            node = stack.pop()
+            if not node.left:
+                store[node] = node.val == 1
+            else:
+                if node.left in store:
+                    store[node] = store[node.left] or store[node.right] if node.val == 2 else  store[node.left] and store[node.right]
+                else:
+                    stack.extend([node, node.left, node.right])
+        
+        return store[root]
