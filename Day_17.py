38712390e65faceb87179d1599c7e4e41f033987
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-05-17 16:47:53 +0530

    Update Day_17.py

--- Day_17.py
+++ Day_17.py
@@ -1,6 +1,8 @@
 # Problem link: https://leetcode.com/problems/delete-leaves-with-a-given-value/description
 # Time: O(n)
 # Space: O(n)
+
+# Recursive approach
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
 
@@ -16,3 +18,35 @@ class Solution:
             return node
     
         return dfs(root)
+
+# Iterative approach
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        stack, visit, parents = [root], set(), {root : None}
+
+        while stack:
+            node = stack.pop()
+
+            if not node.left and not node.right and node.val == target:
+                prt = parents[node]
+                if not prt:
+                    return None
+                
+                if prt.left == node:
+                    prt.left = None
+                elif prt.right == node:
+                    prt.right = None
+                
+            elif node not in visit:
+                visit.add(node)
+                stack.append(node)
+
+                if node.left:
+                    stack.append(node.left)
+                    parents[node.left] = node
+                    
+                if node.right:
+                    stack.append(node.right)
+                    parents[node.right] = node
+
+        return root
